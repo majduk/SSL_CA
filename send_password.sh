44c3b99bbd095306f0141df581c1375994d4d760
Michał Ajduk <majduk@gmail.com>
2015-11-23 17:18:11 +0100

    Delete send_password.sh

--- send_password.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-usage="Usage: $0 msisdn password"
-
-user=${1:? $usage }
-pass=${2:? $usage }
-
-wget --no-proxy --output-document=/dev/null "http://kannel:80/cgi-bin/sendsms?username=${smsuse}&password=${smspass}&from=${from}&to=${user}&text=${pass}"
-
